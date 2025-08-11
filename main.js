e4ad16b985db844857e7574f2be08b8e0ea12532
RandryStephan0410 <randrianasystephanoon@gmail.com>
2025-08-11 03:41:43 +0300

    update

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-console.log('Hello World!');
+const bgAnimation = document.getElementById
+('bgAnimation');
+
+const numberOfColorBoxes = 400;
+
+for (let i = 0; i < numberOfColorBoxes; i++) {
+    const colorBox = document.createElement
+    ('div');
+    colorBox.classList.add('colorBox');
+    bgAnimation.append(colorBox)
+}
